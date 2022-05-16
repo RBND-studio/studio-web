8bb95e04140b33bf834269cbf76b71fec7f5f174
OPesicka <ondrejpesicka@gmail.com>
2022-05-16 19:20:07 +0200

    feat: big bang

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="rbnd studio"
+        description="Rbnd studio is a digital studio based in Prague, Czechia. We create things."
+        openGraph={{
+          type: "website",
+          title: "rbnd studio",
+          url: "https://rbnd.studio/",
+          description: "Rbnd studio is a digital studio based in Prague, Czechia. We create things.",
+        }}
+        additionalMetaTags={[
+          { name: "author", content: "Ondřej Pešička, Vojtěch Vidra" },
+          {
+            name: "keywords",
+            content: "product desgin, ux, ui, design, digital design, startups, development",
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,69 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-
 export default function IndexPage() {
   return (
-    <>
-      <h1>rbdn.studio</h1>
-    </>
+    <div className="container">
+      <div className="circle" />
+      <h1>rbnd studio</h1>
+      <p>We create things</p>
+      <ul>
+        <li>
+          <a href="https://atmos.style/">Atmos.style</a>
+        </li>
+        <li>
+          <a href="https://atmos.style/">Content master - Figma plugin</a>
+        </li>
+      </ul>
+      <footer className="pinda">hello@rbnd.studio</footer>
+      <style jsx>{`
+        h1 {
+          font-weight: 900;
+          font-size: 58px;
+          line-height: 72px;
+          margin: 0;
+        }
+        p {
+          font-weight: 400;
+          font-size: 20px;
+          line-height: 28px;
+          margin: 0;
+        }
+        a {
+          font-weight: 400;
+          font-size: 20px;
+          line-height: 28px;
+          color: #7989fe;
+        }
+        a:hover {
+          color: #576df3;
+        }
+        ul {
+          list-style: none;
+          padding: 0;
+          margin: 0;
+        }
+        li:not(:last-child) {
+          margin-bottom: 8px;
+        }
+        .pinda {
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 20px;
+          color: #77728a;
+        }
+        .container {
+          padding: 120px 16px;
+          display: flex;
+          gap: 24px;
+          flex-direction: column;
+          text-align: center;
+          align-items: center;
+        }
+        .circle {
+          width: 120px;
+          height: 120px;
+          border-radius: 50%;
+          background: linear-gradient(36.98deg, #4053d3 13.16%, #576df3 85.57%);
+        }
+      `}</style>
+    </div>
   );
 }
