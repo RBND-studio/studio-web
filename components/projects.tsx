e44a6ca0d7905748d53088042479c705d74f544d
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-09-19 11:11:32 +0200

    fix: accessibility issues

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,19 +2,16 @@ import { projectData } from "../res/data";
 
 export const Projects = () => {
   return (
-    <ul>
+    <ul className="project-list">
       {projectData.map((key) => (
-        <a
-          key={key.title}
-          href={key.href}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <li className="project">
-            <h2>{key.title}</h2>
-            <p>{key.description}</p>
-          </li>
-        </a>
+        <li key={key.title}>
+          <a href={key.href} target="_blank" rel="noopener noreferrer">
+            <div className="project">
+              <h2>{key.title}</h2>
+              <p>{key.description}</p>
+            </div>
+          </a>
+        </li>
       ))}
     </ul>
   );
